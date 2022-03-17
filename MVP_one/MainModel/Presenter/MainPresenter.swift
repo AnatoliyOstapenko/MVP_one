21cb0bde9026270e6db013ea89e2621f5e81baf1
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-17 15:58:02 +0200

    Update main presenter

--- MVP_one/MainModel/Presenter/MainPresenter.swift
+++ MVP_one/MainModel/Presenter/MainPresenter.swift
@@ -10,28 +10,40 @@ import Foundation
 // set AnyObject word after protocol to make weak reference
 // MainViewProtocol is abstraction from Dependency Inversion (SOLID)
 protocol MainViewProtocol: AnyObject {
-    func setGreeting(greeting: String)
+    func succes()
+    func failure(error: Error)
 }
 
 protocol MainViewPresenterProtocol: AnyObject {
-    init(view: MainViewProtocol, person: FakeAPI)
-    func showGreeting()
+    init(view: MainViewProtocol, networkService: NetworkServiceProtocol)
+    func getData()
+    var array: [FakeAPI?] { get set }
 }
 
 class MainPresenter: MainViewPresenterProtocol {
-    let view: MainViewProtocol
-    let person: FakeAPI
     
-    required init(view: MainViewProtocol, person: FakeAPI) {
+    var array: [FakeAPI?] = []
+    weak var view: MainViewProtocol? // set weak reference to avoid of memory leak
+    let networkService: NetworkServiceProtocol
+    
+    required init(view: MainViewProtocol, networkService: NetworkServiceProtocol) {
         self.view = view
-        self.person = person
+        self.networkService = networkService
     }
-    
-    func showGreeting() {
-        let greeting = self.person.firstName + " " + self.person.lastName
-        self.view.setGreeting(greeting: greeting)
+
+    func getData() {
+        networkService.getData { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let arrayFromNetwork):
+                    self?.array = arrayFromNetwork
+                    self?.view?.succes()
+                case .failure(let error):
+                    self?.view?.failure(error: error)
+                }
+            }
+            
+        }
     }
-    
-    
-    
+
 }
--- MVP_one/Services/NetworkService.swift
+++ MVP_one/Services/NetworkService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol NetworkServiceProtocol {
+    func getData(completion: @escaping(Result <[FakeAPI?], Error>) -> Void)
+}
+
+class NetworkService: NetworkServiceProtocol {
+    
+    func getData(completion: @escaping (Result<[FakeAPI?], Error>) -> Void) {
+        let urlString = "https://jsonplaceholder.typicode.com/comments"
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil, let data = data else { return }
+            do {
+                let data = try JSONDecoder().decode([FakeAPI].self, from: data)
+                completion(.success(data))
+            } catch { completion(.failure(error))}
+        }
+        task.resume()
+    }
+    
+    
+}
